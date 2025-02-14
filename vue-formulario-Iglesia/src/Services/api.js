3b58e73b8f5883725da155e52f0a1138f40966e0
DevHWsolutions <hans.gonzalez@hwsolutions.com.gt>
2025-02-13 19:57:55 -0600

    Feature: Se ha modificado ruta de produccion en la capa de servicio

--- vue-formulario-Iglesia/src/Services/api.js
+++ vue-formulario-Iglesia/src/Services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 //desahws-001-site1.qtempurl.com/
 export default axios.create({
-  baseURL: "http://localhost:5003",
-  // baseURL: "https://desahws-001-site1.qtempurl.com/",
+  // baseURL: "http://localhost:5003",
+  baseURL: "https://desahws-001-site1.qtempurl.com/",
 
   timeout: 5000,
   headers: { "Content-Type": "application/json" },
